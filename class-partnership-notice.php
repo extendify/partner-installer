cbfff9a7f84d48ddd9309458c8b79c2f5f469298
Kevin Batdorf <kbat82@gmail.com>
2022-03-02 00:56:32 -0500

    remove 100 width on svg

--- class-partnership-notice.php
+++ class-partnership-notice.php
@@ -226,7 +226,7 @@ if ( ! class_exists( 'Extendify_Partner', false ) ) {
 							_wpnonce: '<?php echo $nonce; // phpcs:ignore WordPress.Security.EscapeOutput ?>'
 							}
 							);">
-						<svg width="15" height="15" style="width:100%" xmlns="http://www.w3.org/2000/svg" fill="none"
+						<svg width="15" height="15" xmlns="http://www.w3.org/2000/svg" fill="none"
 							viewBox="0 0 24 24" stroke="currentColor">
 							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
 						</svg>
